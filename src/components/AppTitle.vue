132ed12f39f6feb9d4efcde50753ee976a245ca2
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-11-14 16:09:35 +0100

    add all

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,32 @@
+  <script>
+  export default {
+  name:'AppTitle'
+  }
+  </script>
+
+
+<template >
+<div class="cont">
+  <h1>La mia prima app con Vite!</h1>
+</div>
+</template>
+
+
+<style scoped>
+.cont{
+  height: 300px;
+  width: 700px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  
+}
+
+.cont h1{
+  color: #fff;
+  font-size: 2rem;
+  text-shadow: 0px 0px 20px #fff;
+}
+
+</style>
---
+++ src/components/SubTitle.vue
@@ -0,0 +1,33 @@
+  <script>
+  export default {
+  name:'SubTitle'
+  
+  }
+
+  </script>
+
+
+<template>
+
+  <div class="cont">
+      <h2>Si comincia</h2>
+  </div>
+
+
+</template>
+
+
+<style scoped>
+
+.cont{
+  color: #fff;
+  width: 30%;
+  margin: auto;
+  text-align: center; 
+}
+
+.cont h2{
+  text-shadow: 0px 0px 20px;
+}
+
+</style>
